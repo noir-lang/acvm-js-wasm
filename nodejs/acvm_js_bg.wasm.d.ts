15662eefb304eb79eb84497302da8ea83d6a7b63
dbanks12 <david@aztecprotocol.com>
2023-08-16 17:41:58 +0000

    cleanup

--- nodejs/acvm_js_bg.wasm.d.ts
+++ nodejs/acvm_js_bg.wasm.d.ts
@@ -1,23 +1,23 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function buildInfo(): number;
-export function __wbg_simulatedbackend_free(a: number): void;
-export function newSimulatedBackend(): number;
-export function executeCircuit(a: number, b: number, c: number, d: number, e: number): number;
-export function compressWitness(a: number, b: number): void;
-export function decompressWitness(a: number, b: number, c: number): void;
-export function initLogLevel(a: number): void;
-export function getReturnWitness(a: number, b: number, c: number, d: number): void;
-export function getPublicParametersWitness(a: number, b: number, c: number, d: number): void;
 export function getPublicWitness(a: number, b: number, c: number, d: number): void;
+export function getPublicParametersWitness(a: number, b: number, c: number, d: number): void;
+export function getReturnWitness(a: number, b: number, c: number, d: number): void;
+export function buildInfo(): number;
+export function executeCircuit(a: number, b: number, c: number, d: number, e: number): number;
+export function createBackend(): number;
+export function __wbg_simulatedbackend_free(a: number): void;
+export function decompressWitness(a: number, b: number, c: number): void;
+export function compressWitness(a: number, b: number): void;
+export function initLogLevel(a: number): void;
 export function __wbg_trap_free(a: number): void;
 export function trap___wbgd_downcast_token(): number;
 export function __wbindgen_malloc(a: number, b: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__haa6bd105dfd52e9c(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha085f96613bafb2f(a: number, b: number, c: number): void;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_free(a: number, b: number, c: number): void;
 export function __wbindgen_exn_store(a: number): void;
-export function wasm_bindgen__convert__closures__invoke2_mut__h50afad692ca50225(a: number, b: number, c: number, d: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h27f35f552d73468c(a: number, b: number, c: number, d: number): void;
